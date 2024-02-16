f10c875f7bac532f102ca72369133e8ccdaf0280
pe4enka22 <missis.bug@gmail.com>
2024-02-16 20:20:20 +0200

    fix function

--- cypress/e2e/itemOrder.cy.js
+++ cypress/e2e/itemOrder.cy.js
@@ -1,25 +1,26 @@
 describe('Order Item', () => {
-  it('search, add to cart', () => {
-    cy.visit('/');
-    cy.get('#filter_keyword').type('E');
-    cy.get('.button-in-search').click();
+    it('search, add to cart', () => {
+        cy.visit('/');
+        cy.get('#filter_keyword').type('E');
+        cy.get('.button-in-search').click();
 
 
+        function findItem(name){
+            cy.log('search item');
+            cy.get('body').then((body)=>{
 
-function findItem(name) {
-   cy.log('search item');
-   if (cy.contains('.fixed_wrapper .prdocutname', name)) {
-     cy.get(`[title="${name}"]`).click();
-     cy.get('.productname .bgnone').should('have.prop', 'textContent', `${name}`);
-     cy.get('.productpagecart');
-   } else {
-     cy.get('.pagination').contains('>').click();
-     findItem(name);
-   }
- }
-    findItem("Nail Lacquer");
- })
+                if(body.find(`[title="${name}"]`).length>0){
+                    cy.get(`[title="${name}"]`).click();
+                    cy.get('.productname .bgnone').should('have.prop', 'textContent', `${name}`);
+                    cy.get('.productpagecart');
+                } else {
+                    cy.get('.pagination').contains('>').click();
+                    findItem(name);
+                }
 
-})
+            });
+        }
+        findItem('Men+Care Active Clean Shower Tool');
 
-
+    });
+});
