93ab5de623497dcf7ef32cdb9165932a78a5bf76
pe4enka22 <missis.bug@gmail.com>
2024-02-16 15:09:59 +0200

    positive

--- cypress/e2e/itemOrder.cy.js
+++ cypress/e2e/itemOrder.cy.js
@@ -1,5 +1,25 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Order Item', () => {
+  it('search, add to cart', () => {
+    cy.visit('/');
+    cy.get('#filter_keyword').type('E');
+    cy.get('.button-in-search').click();
+
+
+
+function findItem(name) {
+   cy.log('search item');
+   if (cy.contains('.fixed_wrapper .prdocutname', name)) {
+     cy.get(`[title="${name}"]`).click();
+     cy.get('.productname .bgnone').should('have.prop', 'textContent', `${name}`);
+     cy.get('.productpagecart');
+   } else {
+     cy.get('.pagination').contains('>').click();
+     findItem(name);
+   }
+ }
+    findItem("Nail Lacquer");
+ })
+
 })
+
+
